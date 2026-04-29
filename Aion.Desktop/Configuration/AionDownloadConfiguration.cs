16be5f8fd29f08cb16dcfc990d43a572eff97031
Tom Brewer <tombrewer0@gmail.com>
2026-04-28 19:48:34 -0600

    update updates url to use cdn

--- Aion.Desktop/Configuration/AionDownloadConfiguration.cs
+++ Aion.Desktop/Configuration/AionDownloadConfiguration.cs
@@ -2,5 +2,5 @@ namespace Aion.Desktop.Configuration;
 
 public static class AionDownloadConfiguration
 {
-    public const string UpdateBaseUrl = "https://stmythetechglobal.blob.core.windows.net/releases/aion";
+    public const string UpdateBaseUrl = "https://cdn-endpnt-stmythetechglobal.azureedge.net/releases/aion";
 }
